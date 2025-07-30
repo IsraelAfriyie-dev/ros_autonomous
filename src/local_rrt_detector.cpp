cb9aef65e8dd225ad57c1f964cbed793c29e0d2e
Israel Afriyie <109417233+IsraelAfriyie-dev@users.noreply.github.com>
2025-07-29 20:30:26 -0400

    Update local_rrt_detector.cpp

--- src/local_rrt_detector.cpp
+++ src/local_rrt_detector.cpp
@@ -17,7 +17,6 @@
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
 #include "tf2/exceptions.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
 class LocalRRTDetector : public rclcpp::Node
 {

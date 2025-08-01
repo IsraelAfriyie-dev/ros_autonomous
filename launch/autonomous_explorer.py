e97803ec3b773ce726ef0e586952ece298bc8f0a
Israel Afriyie <109417233+IsraelAfriyie-dev@users.noreply.github.com>
2025-07-31 22:16:38 -0400

    Update autonomous_explorer.py

--- launch/autonomous_explorer.py
+++ launch/autonomous_explorer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
-from launch.actions import Node, IncludeLaunchDescription
+from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
-
 
 def generate_launch_description():
     model = LaunchConfiguration('model')
